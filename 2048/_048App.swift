7a2dc9465f2ca2a78d2a61addafb2e111a8bd453
Denis Fouchard <dffouchard@gmail.com>
2025-06-04 23:44:56 +0100

    Initial Commit

---
+++ 2048/_048App.swift
@@ -0,0 +1,17 @@
+//
+//  _048App.swift
+//  2048
+//
+//  Created by Denis Fouchard on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct _048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
